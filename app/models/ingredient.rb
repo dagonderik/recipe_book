e62778dfac105b49cd84c6adfa066e8efa4f1095
dagonderik <dagonderik@gmail.com>
2023-07-04 13:44:52 -0300

    Adds association between ingredient and recipe

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,3 @@
 class Ingredient < ApplicationRecord
-  has_and_belongs_to_many :recipe
+  has_and_belongs_to_many :recipes
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
+  has_and_belongs_to_many :ingredients
+
   validates :title, presence: true
   validates :description, presence: true
 end
