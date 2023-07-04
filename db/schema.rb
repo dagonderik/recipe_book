49b1903f8302a25f36ae9cc741a9e6e4c225ad43
dagonderik <dagonderik@gmail.com>
2023-07-04 13:39:30 -0300

    Adds the Ingredient model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_04_133735) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_04_163223) do
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.integer "recipe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
+  end
+
   create_table "recipes", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,4 +26,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_04_133735) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "ingredients", "recipes"
 end
